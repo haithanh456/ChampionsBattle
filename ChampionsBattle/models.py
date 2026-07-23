be6d1a4cafbcb82752498c6a3f4cd1860f9f8095
Prodigy Zipzap <thanh.tran@muivi.com>
2026-07-23 12:07:33 +0700

    Update models.py

--- ChampionsBattle/models.py
+++ ChampionsBattle/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from bd_models.models import Ball
 
 class ChampionTeam(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -10,3 +11,14 @@ class ChampionTeam(models.Model):
     class Meta:
         verbose_name = "Champion Team"
         verbose_name_plural = "Champion Teams"
+
+class ChampionTeamBall(models.Model):
+    team = models.ForeignKey(ChampionTeam, on_delete=models.CASCADE, related_name="balls")
+    ball = models.ForeignKey(Ball, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.team.name} - {self.ball.country}"
+
+    class Meta:
+        verbose_name = "Team Ball"
+        verbose_name_plural = "Team Balls"
