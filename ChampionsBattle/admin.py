eb35be515056eb51c782be2cb17ccf31c24b7518
Prodigy Zipzap <thanh.tran@muivi.com>
2026-07-23 12:08:00 +0700

    Update admin.py

--- ChampionsBattle/admin.py
+++ ChampionsBattle/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import ChampionTeam
+from .models import ChampionTeam, ChampionTeamBall
+
+class ChampionTeamBallInline(admin.TabularInline):
+    model = ChampionTeamBall
+    extra = 1
+    autocomplete_fields = ("ball",)
 
 @admin.register(ChampionTeam)
 class ChampionTeamAdmin(admin.ModelAdmin):
     list_display = ("name", "is_active")
     list_editable = ("is_active",)
+    inlines = [ChampionTeamBallInline]
