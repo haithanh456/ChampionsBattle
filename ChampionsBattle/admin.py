158a7d019cb72d7f05069d677812eb3b1bc2e7fa
Prodigy Zipzap <thanh.tran@muivi.com>
2026-07-23 11:58:51 +0700

    Update admin.py

--- ChampionsBattle/admin.py
+++ ChampionsBattle/admin.py
@@ -1,22 +1,7 @@
 from django.contrib import admin
-from .models import ChampionTeam, ChampionTeamBall, PlayerEquippedTeam
-
-class ChampionTeamBallInline(admin.TabularInline):
-    model = ChampionTeamBall
-    extra = 1
-    autocomplete_fields = ("ball",)
-    ordering = ("order",)
+from .models import ChampionTeam
 
 @admin.register(ChampionTeam)
 class ChampionTeamAdmin(admin.ModelAdmin):
-    list_display = ("name", "is_active", "created_at")
-    list_filter = ("is_active",)
-    search_fields = ("name",)
+    list_display = ("name", "is_active")
     list_editable = ("is_active",)
-    inlines = [ChampionTeamBallInline]
-
-@admin.register(PlayerEquippedTeam)
-class PlayerEquippedTeamAdmin(admin.ModelAdmin):
-    list_display = ("player", "team", "equipped_at")
-    search_fields = ("player__discord_id", "team__name")
-    autocomplete_fields = ("player", "team")
